7ddbd9de6cc5a737d35ea82d9456f62c83386355
jvinyard <jvinyard@localhost>
2012-03-21 15:36:29 -0500

    - Preprocess class is now an abstract base class - Removed preprocess.py.orig. Not sure where it came from

--- data/learn.py
+++ data/learn.py
@@ -1,15 +1,22 @@
 from controller import Controller
+from abc import ABCMeta,abstractmethod
 
 class LearningController(Controller):
     '''
-    An "abstract" base class for controllers that will persist and fetch
+    An abstract base class for controllers that will persist and fetch
     learning pipelines
     '''
+    
+    __metaclass__ = ABCMeta
+    
+    
     def __init__(self):
         Controller.__init__(self)
-        
+    
+    @abstractmethod    
     def __getitem__(self,key):
         raise NotImplemented()
     
+    @abstractmethod
     def __setitem__(self,key,value):
         raise NotImplemented()
