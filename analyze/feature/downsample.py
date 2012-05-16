863422aa5c29e8ff8365b78da544345fc3f68b01
jvinyard <jvinyard@localhost>
2012-05-16 16:01:48 -0500

    Fixed bug in Downsample whereby 2d features were returned instead of flattened features

--- analyze/feature/downsample.py
+++ analyze/feature/downsample.py
@@ -5,18 +5,20 @@ from analyze.extractor import SingleInput
 
 class Downsample(SingleInput):
     
-    def __init__(self,size,factor,needs = None, key = None):
-        SingleInput.__init__(needs = needs,key = key)
+    def __init__(self,size = None,factor = None ,needs = None, key = None):
+        SingleInput.__init__(self,needs = needs,key = key)
         self.size = size
         self.factor = factor
         
     def dim(self,env):
-        return downsampled_shape(self.size,self.factor)
+        return np.product(downsampled_shape(self.size,self.factor))
     
     @property
     def dtype(self):
         return np.float32
     
     def _process(self):
-        return downsample(self.in_data[0],self.factor)
+        data = self.in_data[0].reshape(self.size)
+        return downsample(data,self.factor).ravel()
+        
     
