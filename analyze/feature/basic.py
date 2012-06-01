138979e22d19dc9ee9bdd73295344a281d6a1cbd
jvinyard <jvinyard@localhost>
2012-06-01 16:06:00 -0500

    Added analyze.feature.basic for "primitives", like sum, absolute value, etc.

---
+++ analyze/feature/basic.py
@@ -0,0 +1,37 @@
+import numpy as np
+from analyze.extractor import SingleInput
+
+
+class Basic(SingleInput):
+    
+    def __init__(self, inshape = None, outshape = None, op = None, needs = None, \
+                  key = None, nframes = 1, step = 1):
+        
+        SingleInput.__init__(\
+                self,needs = needs, key = key, nframes = nframes, step = step)
+        
+        self._inshape = inshape
+        self._outshape = outshape
+        self._op = op
+        
+    def dim(self,env):
+        return self._outshape
+    
+    @property
+    def dtype(self):
+        return np.float32
+    
+    def _process(self):
+        data = np.reshape(self.in_data[:self.nframes],self._inshape)
+        return self._op(data)
+
+
+
+class Abs(Basic):
+    
+    def __init__(self, inshape = None, needs = None, key = None, nframes = 1, step = 1):
+        
+        Basic.__init__(self,inshape = inshape, outshape = np.product(inshape),
+                       op = np.abs, needs = needs, key = key, 
+                       nframes = nframes, step = step)
+        
