bca279417de2711e4e0d7c9ec6a9860bc71f5f1b
jvinyard <jvinyard@localhost>
2012-06-05 12:32:10 -0500

    Added learn.normalize.Normalize, which learns to normalize data on a per-feature basis

---
+++ learn/normalize.py
@@ -0,0 +1,28 @@
+from __future__ import division
+import numpy as np
+
+from learn import Learn
+from environment import Environment
+
+class Normalize(Learn):
+    '''
+    Normalize features based on averages over a representative sample.  In other 
+    words, try to remove bias on a per-feature basis.
+    '''
+    
+    def __init__(self,feature):
+        Learn.__init__(self)
+        self._feature = feature
+        c = Environment.instance.framecontroller
+        self._vec = np.zeros(c.get_dim(self._feature))
+    
+    def train(self,data,stopping_condition):
+        l = len(Environment.instance.framecontroller)
+        for d in data:
+            self._vec += d / l
+        self._vec = 1 / self._vec
+    
+    def __call__(self,data):
+        return data * self._vec
+        
+        
