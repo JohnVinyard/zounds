f96540190e3c807b85e9039f99edfcb859ae4620
jvinyard <jvinyard@localhost>
2012-07-16 15:28:54 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+      name = 'Zounds',
+      version = '1.0',
+      url = 'http://www.johnvinyard.com',
+      author = 'John Vinyard',
+      author_email = 'john.vinyard@gmail.com',
+      packages = ['']
+)
