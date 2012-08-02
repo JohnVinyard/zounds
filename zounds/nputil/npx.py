9e5007dcc350ec3d13de3ac1452f2d56dc1ab802
jvinyard <jvinyard@localhost>
2012-08-02 13:02:13 -0500

    Finished nputil.windowed method and tests

--- zounds/nputil/npx.py
+++ zounds/nputil/npx.py
@@ -47,132 +47,90 @@ def pad(a,desiredlength):
 
 
 def _wpad(l,windowsize,stepsize):
-    end = l - windowsize
+    '''
+    Parameters
+        l - The length of the input array
+        windowsize - the size of each window of samples
+        stepsize - the number of samples to move the window each step
+    
+    Returns
+        The length the input array should be so that no samples are leftover
+    '''
     if l <= windowsize:
         return  windowsize
-    elif l % stepsize:
-        left = l%stepsize
-        diff = stepsize - left
-        return l + diff
-    else:
-        return l
+    end = (((l - windowsize) // stepsize) * stepsize) + windowsize
+    leftover = l - end
+    return l + (stepsize - leftover) if leftover else l
 
 def _wcut(l,windowsize,stepsize):
+    '''
+    Parameters
+        l - The length of the input array
+        windowsize - the size of each window of samples
+        stepsize - the number of samples to move the window each step
+    
+    Returns
+        The length the input array should be so that leftover samples are ignored
+    '''
     end = l - windowsize
     if l <= windowsize:
         return 0
     elif end % stepsize:
-        return windowsize + (((end//stepsize))*stepsize)
+        return windowsize + ((end//stepsize)*stepsize)
     else:
         return l
         
          
 def windowed(a,windowsize,stepsize = None,dopad = False):
-    l = a.shape[0]
+    '''
+    Parameters
+        a          - the input array to restructure into overlapping windows
+        windowsize - the size of each window of samples
+        stepsize   - the number of samples to shift the window each step. If not
+                     specified, this defaults to windowsize
+        dopad      - If false (default), leftover samples are returned seperately.
+                     If true, the input array is padded with zeros so that all
+                     samples are used. 
+    '''
+    
+    if windowsize < 1:
+        raise ValueError('windowsize must be greater than or equal to one')
+    
     if stepsize is None:
         stepsize = windowsize
+    
+    if stepsize < 1:
+        raise ValueError('stepsize must be greater than or equal to one')
+    
+    if windowsize == 1 and stepsize == 1:
+        # A windowsize and stepsize of one mean that no windowing is necessary.
+        # Return the array unchanged.
+        return np.zeros(0),a
+    
+    if windowsize == 1 and stepsize > 1:
+        return np.zeros(0),a[::stepsize]
+    
+    # the original length of the input array
+    l = a.shape[0]
+    
     if dopad:
+        # pad the array with enough zeros so that there are no leftover samples
         a = pad(a,_wpad(l,windowsize,stepsize))
+        # no leftovers; an empty array
         leftover = np.zeros(0)
     else:
+        # cut the array so that any leftover samples are returned seperately
         c = _wcut(l,windowsize,stepsize)
         leftover = a[c:]
         a = a[:c]
-        
-    print a.shape
-    n = 1+(l-windowsize)//(stepsize)
+    
+    if 0 == a.shape[0]:
+        return leftover,np.zeros(0)
+
+    
+    n = 1+(a.shape[0]-windowsize)//(stepsize)
     s = a.strides[0]
     newshape = (n,windowsize)+a.shape[1:]
-    print newshape
     newstrides = (stepsize*s,s) + a.strides[1:]
     return leftover,np.ndarray.__new__(\
             np.ndarray,strides=newstrides,shape=newshape,buffer=a,dtype=a.dtype)     
-        
-    
-import numpy as N
-
-def segment_axis(a, length, overlap=0, axis=None, end='cut', endvalue=0):
-    """Generate a new array that chops the given array along the given axis into overlapping frames.
-
-    example:
-    >>> segment_axis(arange(10), 4, 2)
-    array([[0, 1, 2, 3],
-           [2, 3, 4, 5],
-           [4, 5, 6, 7],
-           [6, 7, 8, 9]])
-
-    arguments:
-    a       The array to segment
-    length  The length of each frame
-    overlap The number of array elements by which the frames should overlap
-    axis    The axis to operate on; if None, act on the flattened array
-    end     What to do with the last frame, if the array is not evenly
-            divisible into pieces. Options are:
-
-            'cut'   Simply discard the extra values
-            'wrap'  Copy values from the beginning of the array
-            'pad'   Pad with a constant value
-
-    endvalue    The value to use for end='pad'
-
-    The array is not copied unless necessary (either because it is 
-    unevenly strided and being flattened or because end is set to 
-    'pad' or 'wrap').
-    """
-
-    if axis is None:
-        a = N.ravel(a) # may copy
-        axis = 0
-
-    l = a.shape[axis]
-
-    if overlap>=length:
-        raise ValueError, "frames cannot overlap by more than 100%"
-    if overlap<0 or length<=0:
-        raise ValueError, "overlap must be nonnegative and length must be positive"
-
-    if l<length or (l-length)%(length-overlap):
-        if l>length:
-            roundup = length + (1+(l-length)//(length-overlap))*(length-overlap)
-            rounddown = length + ((l-length)//(length-overlap))*(length-overlap)
-        else:
-            roundup = length
-            rounddown = 0
-        assert rounddown<l<roundup
-        assert roundup==rounddown+(length-overlap) or (roundup==length and rounddown==0)
-        a = a.swapaxes(-1,axis)
-
-        if end=='cut':
-            a = a[...,:rounddown]
-        elif end in ['pad','wrap']: # copying will be necessary
-            s = list(a.shape)
-            s[-1]=roundup
-            b = N.empty(s,dtype=a.dtype)
-            b[...,:l] = a
-            if end=='pad':
-                b[...,l:] = endvalue
-            elif end=='wrap':
-                b[...,l:] = a[...,:roundup-l]
-            a = b
-        
-        a = a.swapaxes(-1,axis)
-
-
-    l = a.shape[axis]
-    if l==0:
-        raise ValueError, "Not enough data points to segment array in 'cut' mode; try 'pad' or 'wrap'"
-    assert l>=length
-    assert (l-length)%(length-overlap) == 0
-    n = 1+(l-length)//(length-overlap)
-    s = a.strides[axis]
-    newshape = a.shape[:axis]+(n,length)+a.shape[axis+1:]
-    newstrides = a.strides[:axis]+((length-overlap)*s,s) + a.strides[axis+1:]
-
-    try: 
-        return N.ndarray.__new__(N.ndarray,strides=newstrides,shape=newshape,buffer=a,dtype=a.dtype)
-    except TypeError:
-        warnings.warn("Problem with ndarray creation forces copy.")
-        a = a.copy()
-        # Shape doesn't change but strides does
-        newstrides = a.strides[:axis]+((length-overlap)*s,s) + a.strides[axis+1:]
-        return N.ndarray.__new__(N.ndarray,strides=newstrides,shape=newshape,buffer=a,dtype=a.dtype)
--- zounds/nputil/test_nputil.py
+++ zounds/nputil/test_nputil.py
@@ -1,10 +1,120 @@
 import unittest
 from toeplitz import toeplitz2dc as toep2d
 import numpy as np
+from npx import windowed
 
 class NpUtilTest(unittest.TestCase):
+    
     def test_toeplitz(self):
         a = np.zeros((60,100),dtype = np.float32)
         shape = (30,3)
         t = toep2d(a,shape)
         self.assertEqual((30*97,30*3),t.shape)
+    
+
+class WindowedTest(unittest.TestCase):
+    
+    def test_windowsize_ltone(self):
+        a = np.arange(10)
+        self.assertRaises(ValueError, lambda : windowed(a,0,1))
+    
+    def test_stepsize_ltone(self):
+        a = np.arange(10)
+        self.assertRaises(ValueError, lambda : windowed(a,1,0))
+    
+    def test_no_windowing(self):
+        a = np.arange(10)
+        l,w = windowed(a,1,1)
+        self.assertTrue(a is w)
+        self.assertEqual(0,l.shape[0])
+    
+    def test_drop_samples(self):
+        a = np.arange(10)
+        l,w = windowed(a,1,2)
+        self.assertEqual(5,w.shape[0])
+        self.assertEqual(0,l.shape[0])
+    
+    def test_windowsize_two_stepsize_one_cut(self):
+        a = np.arange(10)
+        l,w = windowed(a,2,1)
+        print w
+        self.assertEqual(0,l.shape[0])
+        self.assertEqual((9,2),w.shape)
+    
+    def test_windowsize_two_stepsize_one_pad(self):
+        a = np.arange(10)
+        l,w = windowed(a,2,1,True)
+        print w
+        self.assertEqual(0,l.shape[0])
+        self.assertEqual((9,2),w.shape)
+    
+    def test_windowsize_two_stepsize_two_cut(self):
+        a = np.arange(10)
+        l,w = windowed(a,2,2)
+        self.assertEqual(0,l.shape[0])
+        self.assertEqual((5,2),w.shape)
+    
+    def test_windowsize_two_stepsize_two_pad(self):
+        a = np.arange(10)
+        l,w = windowed(a,2,2,True)
+        self.assertEqual(0,l.shape[0])
+        self.assertEqual((5,2),w.shape)
+    
+    def test_windowsize_three_stepsize_two_cut(self):
+        a = np.arange(10)
+        l,w = windowed(a,3,2)
+        self.assertEqual(1,l.shape[0])
+        self.assertEqual((4,3),w.shape)
+    
+    def test_windowsize_three_stepsize_two_pad(self):
+        a = np.arange(10)
+        l,w = windowed(a,3,2,dopad = True)
+        print w
+        self.assertEqual(0,l.shape[0])
+        self.assertEqual((5,3),w.shape)
+        self.assertTrue(np.all([0,0] == w[-1,-1]))
+    
+    def test_windowsize_three_stepsize_three_cut(self):
+        a = np.arange(10)
+        l,w = windowed(a,3,3)
+        self.assertEqual(1,l.shape[0])
+        self.assertEqual((3,3),w.shape)
+    
+    def test_windowsize_three_stepsize_three_pad(self):
+        a = np.arange(10)
+        l,w = windowed(a,3,3,dopad = True)
+        print w
+        self.assertEqual(0,l.shape[0])
+        self.assertEqual((4,3),w.shape)
+    
+    def test_windowsize_gt_length_cut(self):
+        a = np.arange(5)
+        l,w = windowed(a,6,1)
+        self.assertEqual(5,l.shape[0])
+        self.assertEqual(0,w.shape[0])
+    
+    def test_windowsize_gt_length_pad(self):
+        a = np.arange(5)
+        l,w = windowed(a,6,1,dopad = True)
+        self.assertEqual(0,l.shape[0])
+        self.assertEqual((1,6),w.shape)
+    
+    def test_twod_cut(self):
+        a = np.arange(20).reshape((10,2))
+        l,w = windowed(a,3,2)
+        self.assertEqual(1,l.shape[0])
+        self.assertEqual((4,3,2),w.shape)
+    
+    def test_twod_pad(self):
+        a = np.arange(20).reshape((10,2))
+        l,w = windowed(a,3,2,dopad = True)
+        self.assertEqual(0,l.shape[0])
+        self.assertEqual((5,3,2),w.shape)
+    
+    def test_no_stepsize_specified(self):
+        a = np.arange(10)
+        l,w = windowed(a,2)
+        self.assertEqual(0,l.shape[0])
+        self.assertEqual((5,2),w.shape)
+        
+        
