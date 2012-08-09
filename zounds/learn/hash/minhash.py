767e516441e530cc28ad83d4bb3a7131493b8849
jvinyard <jvinyard@localhost>
2012-08-09 14:30:32 -0500

    MinHash now performs chunk-based processing

--- zounds/learn/hash/minhash.py
+++ zounds/learn/hash/minhash.py
@@ -28,12 +28,17 @@ class MinHash(Learn):
         Produce a hash by finding the first non-zero "word" from each
         permutation of the input
         '''
-        a = np.ndarray(self.nhashes)
-        a[:] = self.size + 1
+        a = np.ndarray((data.shape[0],self.nhashes))
+        # assign a special value that means, "there were no nonzero values"
+        a[...] = self.size + 1
         for i,h in enumerate(self.hashes):
-            try:
-                a[i] = np.where(data[h] == 1)[0][0]
-            except IndexError:
-                pass
+            # get the nonzero indices for this permutation
+            nz = np.nonzero(data[h])
+            # get the unique values from the first dimension of nz, and the
+            # indices at which they appear. This is necessary because we 
+            # want to know the *first* nonzero index, and not all of them.
+            unique,indices = np.unique(nz[0],return_index = True)
+            # For every example, for this permutation, assign the index of the
+            # first non-zero value, if there was one
+            a[:,i][unique] = nz[1][indices]
         return a
-            
