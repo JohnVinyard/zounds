2c64d7a5ffd02a832f9a2520fa297258a6a3287f
jvinyard <jvinyard@localhost>
2012-08-09 13:01:17 -0500

    All Learn-derived classes in the eigen and cluster modules now perform chunk-based processing

--- zounds/learn/cluster/eigen.py
+++ zounds/learn/cluster/eigen.py
@@ -1,9 +1,11 @@
 from __future__ import division
-from zounds.learn.learn import Learn
-import numpy as np
-from zounds.nputil import safe_unit_norm as sun
-from scipy.spatial.distance import cdist
 
+import numpy as np
+from scipy.spatial.distance import cdist
+from sklearn.decomposition import PCA as SKPCA
+
+from zounds.learn.learn import Learn
+from zounds.nputil import safe_unit_norm as sun
 
 class Eigenvectors(Learn):
     
@@ -18,6 +20,21 @@ class Eigenvectors(Learn):
         self.codebook = vectors.T[:self._ncodes]
     
     def __call__(self,data):
-        dist = cdist(np.array([data]),self.codebook)[0]
+        dist = cdist(self.in_data,self.codebook)
         dist[dist == 0] = 1e-3
         return 1 / dist
+
+
+class PCA(Learn):
+    
+    def __init__(self,n_dim):
+        Learn.__init__(self)
+        self.n_dim = n_dim
+        self._pca = None
+    
+    def train(self,data,stopping_condition):
+        self._pca = SKPCA(self.n_dim)
+        self._pca.fit(data)
+    
+    def __call__(self,data):
+        return self._pca.transform(data)
