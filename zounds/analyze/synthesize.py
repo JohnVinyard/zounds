64a2ad2a2a5dd65073583521584a4ab7ae45d7b5
jvinyard <jvinyard@localhost>
2012-08-23 15:35:05 -0500

    Added measures to avoid segmentation fault when encoding large files as ogg vorbis

--- zounds/analyze/synthesize.py
+++ zounds/analyze/synthesize.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scikits.audiolab import play
 
+
 class WindowedAudioSynthesizer(object):
     '''
     A very simple synthesizer that assumes windowed, but otherwise raw frames
@@ -10,13 +11,64 @@ class WindowedAudioSynthesizer(object):
         object.__init__(self)
         self.windowsize = windowsize
         self.stepsize = stepsize
+        
+        # libvoribs 1.3.1 is causing a segmentation fault when writing samples
+        # near this number (regardless of sample rate)
+        self.vorbis_seg_fault_size = 45 * 44100
+        # Writing samples to a ogg vorbis file in chunks eliminates the segfault,
+        # and is faster in some cases (especially for longer files), but introduces
+        # small, but noticeable clicks at the chunk boundaries.  Libvorbis 
+        # seems to not be "crosslapping" at write boundaries. 
+        self.vorbis_chunk_size = 25 * 44100
     
-    def __call__(self,frames):
-        output = np.zeros(self.windowsize + (len(frames) * self.stepsize))
+    def _vorbis_write(self,frames,sndfile,output):
+        cs = self.vorbis_chunk_size
+        waypoint = 0
         for i,f in enumerate(frames):
             start = i * self.stepsize
             stop = start + self.windowsize
             output[start : stop] += f
+            if (stop - waypoint) > cs:
+                # a sndfile was passed in, and we have enough data to write
+                # a chunk of audio to the file
+                sndfile.write_frames(output[waypoint : stop])
+                # update the end of the last chunk written
+                waypoint = stop
+        
+        if sndfile:
+            # write any remaining data to disk
+            sndfile.write_frames(output[waypoint:])
+        
+        return output
+    
+    def __call__(self,frames,sndfile = None):
+        '''
+        Parameters
+            frames  - a two dimensional array containing frames of audio samples
+            sndfile - Optional. A scikits.audiolab.Sndfile instance to which the samples
+                      should be written.
+        
+        Returns
+            output - a 1D array containing the rendered audio
+        '''
+        output = np.zeros(self.windowsize + (len(frames) * self.stepsize))
+        
+        if sndfile and 'ogg' == sndfile.file_format:
+            # The caller has requested that output audio be written to an ogg
+            # vorbis file.  libvorbis 1.3.1 is currently causing segmentation faults
+            # for larger files, so we'll need to handle this specially.
+            return self._vorbis_write(frames, sndfile, output)
+    
+        
+        for i,f in enumerate(frames):
+            start = i * self.stepsize
+            stop = start + self.windowsize
+            output[start : stop] += f
+
+    
+        if sndfile:
+            sndfile.write_frames(output)
+        
         return output
             
     def play(self,audio):
