4bda8a480ed063c84c2c12d661467930bcaf5d6f
jvinyard <jvinyard@localhost>
2012-08-22 19:04:00 -0500

    - Playhead can now be positioned by the user. - spectrogram displays now consist of equally-sized chunks, a la a tile server - Slightly more attractive styling

--- zounds/quickstart/websearch/js/websearch.js
+++ zounds/quickstart/websearch/js/websearch.js
@@ -1,10 +1,18 @@
+var interval = null;
+
+window.onkeydown=function(e){
+  if(e.keyCode==32){
+   return false;
+  }
+};
+
 function focus_query() {
 	$('.snippet:first').focus();
 }
 
 function position_playhead(playhead,snippet) {
 	var a = snippet.find('audio')[0];
-	var img = snippet.find('img');
+	var img = snippet.find('.spectrogram');
 	var offset = img.offset();
 	var width = img.width();
 	var ratio = a.currentTime / a.duration;
@@ -15,34 +23,43 @@ function position_playhead(playhead,snippet) {
 function audio_spacebar(e) {
 	var snippet = $(this);
 	var audio = snippet.find('audio')[0];
-	var img = snippet.find('img');
+	var img = snippet.find('.spectrogram');
 	var playhead = $('#playhead');
+	var pps = img.width() / audio.duration;
+	var update_freq_ms = 50;
+	var increment_px = pps * (update_freq_ms / 1000);
 	
 	audio.addEventListener('ended',function() {
-		console.debug('audio ended');
 		this.pause();
 		this.currentTime = 0;
+		clearInterval(interval);
 		position_playhead(playhead,snippet);
 	},false);
 	
 	audio.addEventListener('timeupdate',function() {
+		clearInterval(interval);
 		position_playhead(playhead,snippet);
+		if(!this.paused){
+			interval = setInterval(function() {
+				var offset = playhead.offset();
+				offset.left += increment_px;
+				playhead.offset(offset);
+			},update_freq_ms);
+		}
 	},false);
 	
 	if(32 == e.keyCode) {
 		if(audio.paused) {
 			position_playhead(playhead,snippet);
-			//var ratio =  audio.currentTime / audio.duration;
-			//playhead.animate({
-			//	left : img.offset().left + img.width()
-			//},(audio.duration - audio.currentTime) * 1000,'linear');
 			audio.play();
 		}else {
 			audio.pause();
-			playhead.stop();
+			clearInterval(interval);
 			audio.removeEventListener('timeupdate');
 		}
+		return false;
 	}
+	
 }
 
 function draw_playhead(jqp) {
@@ -58,17 +75,30 @@ $(function() {
 	draw_playhead(playhead);
 	
 	$('.snippet').focus(function() {
-		position_playhead(playhead,$(this));
-		$(this)
+		var snippet = $(this);
+		position_playhead(playhead,snippet);
+		snippet
 			.addClass('focus')
 			.keyup(audio_spacebar);
+		var img = snippet.find('.spectrogram');
+		img.click(function(e) {
+			var offset = playhead.offset();
+			offset.left = e.pageX;
+			var img_offset = $(this).offset();
+			var ratio = (e.pageX - img_offset.left) / $(this).width();
+			var audio = snippet.find('audio')[0];
+			audio.currentTime = ratio * audio.duration;
+			position_playhead(playhead,snippet);
+		});
 	});
 	
 	$('.snippet').blur(function() {
 		$(this).find('audio')[0].pause();
-		playhead.stop();
+		clearInterval(null);
 		$(this)
 			.removeClass('focus')
 			.unbind('keyup');
+		var img = $(this).find('.spectrogram');
+		img.unbind('click');
 	});
 });
