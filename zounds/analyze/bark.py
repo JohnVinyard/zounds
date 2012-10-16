aab6b33e1f68358c5dfdc26ea898f5cbd506eb5e
jvinyard <jvinyard@localhost>
2012-10-16 17:37:26 -0500

    - Added new psychoacoustics module, which paves the way to generalize mapping from hz to biologically-inspired scales.  This module will eventually replace bark.py - The BarkBands extractor now uses code defined in psychoacoustics.py

--- zounds/analyze/bark.py
+++ zounds/analyze/bark.py
@@ -31,24 +31,7 @@ def _hz_is_valid(freq_hz,sr):
         raise ValueError(\
             'Freq must be greater than zero and less than the Nyquist frequency')
     
-def tri_window(start_hz,stop_hz,ws,sr):
-    # TODO: Alter the edges of the triangle window so they're rounding-aware
-    _hz_is_valid(start_hz,sr)
-    _hz_is_valid(stop_hz,sr)
-    fft_bandwidth = (sr * .5) / (ws * .5)
-    s = freq_hz / fft_bandwidth
-    e = freq_hz / fft_bandwidth
-    s_index = int(np.floor(s))
-    e_index = int(np.ceil(e))
-    e_index = s_index + 1 if s_index == e_index else e_index
-    nbins = e_index - s_index
-    tri = triang(nbins)
-    if 1 == nbins:
-        return tri
-    
-    tri[0] *= (s % 1)
-    tri[-1] *= (e % 1)
-    return s_index,e_index,tri 
+
 
 def hz_to_barks(hz):
     return 6. * np.log((hz/600.) + np.sqrt((hz/600.)**2 + 1))
@@ -89,32 +72,79 @@ def critical_bands(samplerate,
         
     return cb
 
+
+def tri_window(start_hz,stop_hz,ws,sr):
+    # Idea 1: Scale by the max
+    # Idea 2: Scale by the max and the diff
+    
+    # check that the hz values are greater than zero and less than the nyquist
+    # frequency for this sampling rate
+    _hz_is_valid(start_hz,sr)
+    _hz_is_valid(stop_hz,sr)
+    # How many fft bins equal 1hz?
+    fft_bandwidth = (sr * .5) / (ws * .5)
+    # the float-valued fft start bin
+    s = start_hz / fft_bandwidth
+    # the float-valued fft stop bin
+    e = stop_hz / fft_bandwidth
+    # the int-valued, rounded fft start bin
+    s_index = int(np.floor(s))
+    # the int-valued, rounded fft stop bin
+    e_index = int(np.ceil(e))
+    # make sure we're spanning at least one bin
+    e_index = s_index + 1 if s_index == e_index else e_index
+    # how many whole fft bins will this bark band use?
+    nbins = e_index - s_index
+    
+    # a triangular window which we'll multiply the fft bins by, and then sum
+    tri = triang(nbins)
+    # ratio between 1 (the expected peak value for the triangular window) and
+    # the actual value.  For windows with an even number of bins, this will 
+    # always be > 1
+    peak_diff = (1 / tri.max())
+    # the difference between the real-valued fft bin span, and the rounded, 
+    # int-valued fft bin span
+    bin_diff = (e - s) / nbins
+    tri = tri * peak_diff * bin_diff
+    
+    
+    print peak_diff, bin_diff, tri.sum()
+    
+    # how much distortion did we introduce by rounding to a whole number of bins?
+    #scale = (e - s) / nbins
+    # scale the entire window by that much
+    #tri *= scale
+    
+    
+    return s_index,e_index,tri 
+
 def bark_data(samplerate,window_size,nbands,start_freq_hz,stop_freq_hz):
-        start_bark = hz_to_barks(start_freq_hz)
-        stop_bark = hz_to_barks(stop_freq_hz)
-        bark_bandwidth = (stop_bark - start_bark) / nbands
-        # slices of fft coefficients
-        _slices = []
-        # triangle windows to multiply the fft slices by
-        _triwins = []
-        for i in xrange(1,nbands + 1):
-            b = i * bark_bandwidth
-            hz = barks_to_hz(b)
-            _herb = erb(hz) / 2.
-            start_hz = hz - _herb
-            start_hz = 0 if start_hz < 0 else start_hz
-            stop_hz = hz + _herb
-            
-            #s_index,e_index = fft_span(\
-            #                start_hz,stop_hz,window_size,samplerate)
-            
-            #triang_size = e_index - s_index
-            #print triang_size   
-            #triwin = triang(triang_size)
-            s_index,e_index,triwin = tri_window(start_hz,stop_hz,window_size,samplerate)
-            _slices.append(slice(s_index,e_index))
-            _triwins.append(triwin)
-        return _slices,_triwins
+    start_bark = hz_to_barks(start_freq_hz)
+    stop_bark = hz_to_barks(stop_freq_hz)
+    bark_bandwidth = (stop_bark - start_bark) / nbands
+    # slices of fft coefficients
+    _slices = []
+    # triangle windows to multiply the fft slices by
+    _triwins = []
+    for i in xrange(1,nbands + 1):
+        b = i * bark_bandwidth
+        hz = barks_to_hz(b)
+        _herb = erb(hz) / 2.
+        start_hz = hz - _herb
+        start_hz = 0 if start_hz < 0 else start_hz
+        stop_hz = hz + _herb
+        
+        #s_index,e_index = fft_span(\
+        #                start_hz,stop_hz,window_size,samplerate)
+        
+        #triang_size = e_index - s_index
+        #print triang_size   
+        #triwin = triang(triang_size)
+        s_index,e_index,triwin = tri_window(\
+                                start_hz,stop_hz,window_size,samplerate)
+        _slices.append(slice(s_index,e_index))
+        _triwins.append(triwin)
+    return _slices,_triwins
 
 def bark_bands(\
         samplerate,window_size,nbands,start_freq_hz,stop_freq_hz,fft,nframes,
@@ -124,6 +154,3 @@ def bark_bands(\
     for i in xrange(nbands): 
         cb[:,i] = (fft[:,slices[i]] * triwins[i]).sum(1)
     return cb
-
-if __name__ == '__main__':
-    slices,triwins = bark_data(44100,2048,200,50,12000)
---
+++ zounds/analyze/psychoacoustics.py
@@ -0,0 +1,145 @@
+from __future__ import division
+from abc import ABCMeta,abstractmethod
+import numpy as np
+from scipy.signal import triang
+
+def fft_index(freq_hz,ws,sr,rnd = np.round):
+    '''
+    Given a frequency in hz, a window size, and a sample rate,
+    return the fft bin into which the freq in hz falls
+    '''
+    if freq_hz < 0 or freq_hz > sr / 2.: 
+        raise ValueError(\
+            'Freq must be greater than zero and less than the Nyquist frequency')
+    
+    fft_bandwidth = (sr * .5) / (ws * .5)
+    return int(rnd(freq_hz / fft_bandwidth))
+
+def fft_span(start_hz,stop_hz,ws,sr):
+    '''
+    Given a span in hz, return the start and stop fft bin indices covering
+    that span
+    '''
+    s_index = fft_index(start_hz,ws,sr, rnd = np.floor)
+    e_index = fft_index(stop_hz,ws,sr, rnd = np.ceil)
+    # ensure that the span is at least one fft bin
+    e_index = s_index + 1 if s_index == e_index else e_index
+    return s_index,e_index
+
+def _hz_is_valid(freq_hz,sr):
+    if freq_hz < 0 or freq_hz > sr / 2.: 
+        raise ValueError(\
+            'Freq must be greater than zero and less than the Nyquist frequency')
+
+def erb(hz):
+    '''
+    equivalent rectangular bandwidth
+    '''
+    return (0.108 * hz) + 24.7
+
+class Scale(object):
+    
+    __metaclass__ = ABCMeta
+    
+    _data = dict()
+    
+    def __init__(self,samplerate,window_size,nbands,start_freq_hz,stop_freq_hz):
+        object.__init__(self)
+        self._sr = samplerate
+        self._ws = window_size
+        self._nb = nbands
+        self._sfhz = start_freq_hz
+        self._efhz = stop_freq_hz
+        self._data_key = (self.__class__.__name__,
+                          self._sr,self._ws,self._nb,
+                          self._sfhz,self._efhz)
+        
+        if self._data_key not in self._data:
+            self._data[self._data_key] = self._build_data() 
+        
+    @abstractmethod
+    def to_hz(self,s):
+        pass
+    
+    @abstractmethod
+    def from_hz(self,hz):
+        pass
+    
+    @abstractmethod
+    def _build_data(self):
+        pass
+    
+    @property
+    def n_bands(self):
+        return self._nb
+    
+    @property
+    def data(self):
+        return self._data[self._data_key]
+    
+    def transform(self,fft):
+        slices,triwins = self.data
+        cb = np.ndarray((fft.shape[0],self._nb),dtype = np.float32)
+        for i in xrange(self._nb):
+            cb[:,i] = (fft[:,slices[i]] * triwins[i]).sum(1)
+        return cb
+    
+class Bark(Scale):
+    
+    def __init__(self,samplerate,window_size,nbands,start_freq_hz,stop_freq_hz):
+        self._fft_bandwidth = (samplerate * .5) / (window_size * .5)
+        Scale.__init__(\
+                self,samplerate,window_size,nbands,start_freq_hz,stop_freq_hz)
+        
+        
+    
+    def to_hz(self,b):
+        return 300. * ((np.e ** (b/6.0)) - (np.e ** (-b/6.)))
+    
+    def from_hz(self,hz):
+        return 6. * np.log((hz/600.) + np.sqrt((hz/600.)**2 + 1))
+    
+    def _build_data(self):
+        start_bark = self.from_hz(self._sfhz)
+        stop_bark = self.from_hz(self._efhz)
+        bark_bandwidth = (stop_bark - start_bark) / self._nb
+        
+        _slices = []
+        _triwins = []
+        
+        
+        for i in xrange(1,self._nb + 1):
+            b = i * bark_bandwidth
+            hz = self.to_hz(b)
+            _herb = erb(hz) / 2
+            start_hz = hz - _herb
+            start_hz = 0 if start_hz < 0 else start_hz
+            stop_hz = hz + _herb
+            _hz_is_valid(start_hz,self._sr)
+            _hz_is_valid(stop_hz,self._sr)
+            s = start_hz / self._fft_bandwidth
+            e = stop_hz / self._fft_bandwidth
+            s_index = int(np.floor(s))
+            e_index = int(np.ceil(e))
+            e_index = s_index + 1 if s_index == e_index else e_index
+            n_bins = e_index - s_index
+            _slices.append(slice(s_index,e_index))
+            
+            tri = triang(n_bins)
+            pd = (n_bins // 2) / (n_bins / 2) 
+            bd = (e - s) / n_bins
+            
+            # scale the triangular window to account for aliasing introduced by
+            # a) the rounded number of bins in the window
+            # b) the fact that even numbered windows won't have a peak with value
+            #    1 in the middle
+            tri = tri * pd * bd
+            _triwins.append(tri)
+        
+        return _slices,_triwins
+            
+            
+            
+        
+    
+    
