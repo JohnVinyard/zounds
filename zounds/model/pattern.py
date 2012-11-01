d198f2be932f7759a188a730336a026e96a9c168
jvinyard <jvinyard@localhost>
2012-11-01 10:19:57 -0500

    Started working on Pattern tests

--- zounds/model/pattern.py
+++ zounds/model/pattern.py
@@ -13,10 +13,26 @@ Pattern from the outside world, i.e., just some sound file
     address : (100,200) // A PyTables address
 }
 
+ >>> p = Pattern.leaf(addr) // addr can be a frames instance, an address, or a frame id
+ >>> p.store()
+
 She store()s the pattern.
 
 Ann then arranges the kick pattern into one in which the drum plays four times
 in succession. She store()s this pattern.
+
+  >>> p2 = Pattern()
+  >>> p2.append(p,[Event(0,amp = 1),Event(1,amp = .5),...])
+  >>> p2.store()
+  >>> _id = p2._id
+
+
+{
+    _id : 'leaf'
+    source : Ann
+    external_id : 'leaf' 
+    address : (100,200) // A PyTables address
+}
 
 A pattern using that slice
 {
@@ -24,7 +40,7 @@ A pattern using that slice
     source : Ann
     external_id : 'fourquarter' 
     address : None
-    all_ids : ['slice'],
+    all_ids : ['leaf'],
     data : {
         'leaf' : [
             (0, [{'amp' : (1,)}]),
@@ -35,16 +51,20 @@ A pattern using that slice
     }
 }
 
+ 
+
 The client is returned a data structure like the one above.  Notice that address
 is None.  'fourquarter' has been placed into a queue to be analyzed, and doesn't
-yet have a home in the frames database.  
+yet have a home in the frames database.  Notice that 'leaf' is also returned,
+so the pattern can be rendered by the client.  
 
-Ann closes the application, and comes back later.  She fetches 'fourquarter' from
+Ann closes the client application, and comes back later.  She fetches 'fourquarter' from
 the database.  Note that 'fourquarter' now has an address.  This means
 it has been analyzed, and is a candidate for audio-similarity results.  Ann 
 decides she'd like the beats to play only on the 1 and 3.  Note that the 
 'fourquarter' pattern is returned to the client, as well as all patterns it 
 references.
+
 
 ** IDEA ** Note that patterns are an example of an audio encoding.  They can
 be interpreted by a synthesizer. It would be very space efficient to store the
@@ -71,9 +91,14 @@ JSON or BSON data instead of raw audio, in most cases.
     }
 }
 
+ >>> p3 = Pattern[_id]
+ >>> p4 = p3.remove('leaf',[1,3])
+ >>> p4.store()
+ >>> _id = p4._id
+
 She calls store() on this pattern.  When the data is sent back to the server, 
 leaf's hash value is the same, but 'fourquarter's hash value has changed.  'fourquarter'
-is stored as a new pattern 'twoquarter'.  The client is informed that.
+is stored as a new pattern 'twoquarter'.
 
 Ann returns to the client later, and fetches the 'twoquarter' pattern.
 
@@ -117,6 +142,12 @@ to
     address : (100,180) // A PyTables address
 }
 
+ >>> p4 = Pattern[_id]
+ >>> leaf = p4.get('leaf',2)[0]
+ >>> leaf_shorter = leaf.change_address((100,180))
+ >>> p4 = p4.replace((leaf,2),leaf_shorter)
+ >>> p4.store()
+
 When she alters the already stored leaf pattern, the client makes a copy of the
 pattern. When she store()s 'twoquarter', three patterns are sent back to the 
 server:
@@ -154,8 +185,9 @@ server:
 The second leaf pattern has a different hash value than it was sent with, and
 so a new leaf pattern is created.
 
-QUESTION: If the client changes leaf.address directly, how does the "leaf" object
-know to make a copy?
+All changes to a pattern should happen by calling methods on the Pattern class.
+No direct acess to the underlying data structures should be made. Maybe the
+last operation Ann executes would happen like so: 
 '''
 
 
@@ -164,6 +196,7 @@ import numpy as np
 from zounds.model.model import Model
 from zounds.analyze.feature.rawaudio import AudioFromDisk,AudioFromMemory
 from zounds.analyze.synthesize import transformers,TransformChain
+from zounds.environment import Environment
 
 class Pattern(Model):
     '''
@@ -253,9 +286,10 @@ class Zound2(Pattern):
     
     __metaclass__ = MetaZound2
     
+    env = Environment.instance
+    
     def __init__(self,source = None,external_id = None,_id = None,
-                 address = None,data = None,all_ids = None,
-                 bpm = None):
+                 address = None,data = None,all_ids = None,is_leaf = False):
         
         # source is the name of the application or user that created this pattern
         self.source = source or self.env().source
@@ -270,16 +304,42 @@ class Zound2(Pattern):
         if not address:
             raise ValueError('specify one of address or data')
         
-        # TODO: Hash the pattern somehow, so it can be compared to ... itself?
+
         self.address = address
         self.data = data or dict()
         self._sort_events()
         self.all_ids = set(all_ids) or set()
         self._patterns = None
-        
-        self.bpm = bpm
+        self._is_leaf = is_leaf
         
         self.env = self.env()
+    
+    @classmethod
+    def leaf(cls,addr,source = None):
+        '''leaf
+        
+        :param addr: Address can be a Zounds frame id, a Zounds frame address, or \
+        a Frames-derived instance with the address property set
+        '''
+        _id = cls.env.newid()
+        try:
+            a = cls.env.address_class(addr)
+        except ValueError:
+            pass
+        
+        try:
+            a = cls.env.framecontroller.address(addr)
+        except KeyError:
+            pass
+        
+        try:
+            a = addr.address
+        except AttributeError:
+            raise ValueError('addr must be a Zounds address, a Zounds frame _id, or a Frames instance with the address property set')
+        
+        source = source or cls.env.source
+        return Zound2(source = source,address = a,is_leaf = True)
+            
         
     # TODO: Move this into the base Pattern class
     # BUG: What if a transform changes the length of the samples?
