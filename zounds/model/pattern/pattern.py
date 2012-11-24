6e5ce9d2261f8e62e7ff6a2b13511eb6fc2b3eea
jvinyard <jvinyard@localhost>
2012-11-24 16:50:45 -0600

    - Reorganized the zounds.model.pattern module so that it's now a package - Fixed bug whereby serialized Event instances weren't deserialized correctly

---
+++ zounds/model/pattern/pattern.py
@@ -0,0 +1,91 @@
+from __future__ import division
+
+from zounds.model.model import Model
+from zounds.analyze.feature.rawaudio import AudioFromDisk,AudioFromMemory
+from zounds.pattern import usecs,put
+
+class MetaPattern(type):
+    
+    def __init__(self,name,bases,attrs):
+        super(MetaPattern,self).__init__(name,bases,attrs)
+    
+    def __getitem__(self,key):
+        item = self.controller()[key]
+        
+        try:
+            return self.fromdict(item,stored = True)
+        except AttributeError:
+            return [self.fromdict(i,stored = True) for i in item]
+    
+    def _store(self,pattern):
+        self.controller().store(pattern.todict())
+        pattern.stored = True
+
+class Pattern(Model):
+    '''
+    A Pattern is the central concept in zounds.  Patterns can be nested.
+    a "leaf" pattern represents a list of ids which point to audio frames.
+    A "branch" pattern points to other patterns.
+    '''
+    __metaclass__ = MetaPattern
+    
+    def __init__(self,_id,source,external_id):
+        Model.__init__(self)
+        
+        self.source = source
+        self.external_id = external_id
+        self._id = _id
+        self._data = {'source'      : self.source,
+                      'external_id' : self.external_id,
+                      '_id'         : self._id} 
+        
+        self._fc = None
+    
+    @property
+    def framecontroller(self):
+        if not self._fc:
+            self._fc = self.env().framecontroller
+        return self._fc
+    
+    
+    def audio_extractor(self,needs = None):
+        raise NotImplemented()
+    
+    
+    def data(self):
+        return self._data
+
+class FilePattern(Pattern):
+    '''
+    Represents a pattern in the form of an audio file on disk that has not 
+    yet been stored 
+    '''
+    
+    def __init__(self,_id,source,external_id,filename):
+        Pattern.__init__(self,_id,source,external_id)
+        self.filename = filename
+
+    def audio_extractor(self, needs = None):
+        e = self.__class__.env()
+        return AudioFromDisk(e.samplerate,
+                             e.windowsize,
+                             e.stepsize,
+                             self.filename,
+                             needs = needs)
+
+class DataPattern(Pattern):
+    '''
+    Represents a pattern in the form of an in-memory numpy array of audio 
+    samples that has not yet been stored
+    '''
+    def __init__(self,_id,source,external_id,samples):
+        Pattern.__init__(self,_id,source,external_id)
+        self.samples = samples
+        
+    def audio_extractor(self,needs = None):
+        e = self.__class__.env()
+        return AudioFromMemory(e.samplerate,
+                               e.windowsize,
+                               e.stepsize,
+                               self.samples,
+                               needs = needs)
