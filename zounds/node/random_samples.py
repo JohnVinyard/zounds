25d76b94ae9831679a7dacbfd682ded903965343
jvinyard <jvinyard@localhost>
2015-03-27 22:06:26 -0500

    Introduce reservoir sampler (for samples that will fit into memory) and streaming sampler (for samples that won't)

--- zounds/node/random_samples.py
+++ zounds/node/random_samples.py
@@ -1,18 +1,6 @@
 from flow import Node
 import numpy as np
 
-class RandomSamples(Node):
-    
-    def __init__(self, needs = None, probability = None):
-        super(RandomSamples,self).__init__(needs = needs)
-        self._probability = probability
-    
-    def _process(self,data):
-        n_samples = len(data) // self._probability
-        indices = np.random.permutation(len(data))[:n_samples]
-        print data[indices].shape
-        yield data[indices]
-
 class ReservoirSampler(Node):
     '''
     Use reservoir sampling (http://en.wikipedia.org/wiki/Reservoir_sampling) to
@@ -45,7 +33,7 @@ class ReservoirSampler(Node):
         self._index += remaining
     
     def _dequeue(self):
-        if self._finalized: yield self._r
+        if self._finalized: return self._r
 
 class ChunkedStreamingSampler(Node):
     # first, fill the pool in random order by choosing (random pool, random index)
