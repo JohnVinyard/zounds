3e79712effad32ebd046b310d4f6546c5cd5421d
jvinyard <jvinyard@localhost>
2015-08-09 14:10:59 -0500

    Add RBM as an available unsupervised learning algorithm

--- zounds/node/learn.py
+++ zounds/node/learn.py
@@ -2,6 +2,7 @@ from flow import Node
 from preprocess import Op, PreprocessResult, Preprocessor
 from timeseries import ConstantRateTimeSeries
 from scipy.cluster.vq import kmeans
+from zounds.learn.nnet.rbm import Rbm, LinearRbm as LinRbm
 
 class KMeans(Preprocessor):
     
@@ -27,6 +28,37 @@ class KMeans(Preprocessor):
         data = op(data)
         yield PreprocessResult(data, op)
 
+class BaseRbm(Preprocessor):
+    
+    def __init__(self, cls = None, hdim = None, epochs = 100, needs = None):
+        super(BaseRbm, self).__init__(needs = needs)
+        self._hdim = hdim
+        self._epochs = epochs
+        self._cls = cls
+    
+    def _process(self, data):
+        data = self._extract_data(data)
+        rbm = self._cls(indim = data.shape[1], hdim = self._hdim)
+        rbm.train(data, lambda epoch, error : epoch > self._epochs)
+    
+        def x(d, rbm = None):
+            return rbm(d)
+        
+        op = Op(x, rbm = rbm)
+        data = op(data)
+        yield PreprocessResult(data, op)
+    
+class BinaryRbm(BaseRbm):
+    
+    def __init__(self, hdim = None, epochs = 100, needs = None):
+        super(BinaryRbm, self).__init__(\
+                cls = Rbm, hdim = hdim, epochs = epochs, needs = needs)
+
+class LinearRbm(BaseRbm):
+    
+    def __init__(self, hdim = None, epochs = 100, needs = None):
+        super(LinearRbm, self).__init__(\
+                cls = LinRbm, hdim = hdim, epochs = epochs, needs = needs)
 
 class Learned(Node):
     
