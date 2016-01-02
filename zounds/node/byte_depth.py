b34f9de702fe55b46e8b7508f5a5bd6e1d18bb18
jvinyard <jvinyard@localhost>
2016-01-02 10:31:19 -0600

    Update zounds to use newer method of configuration in flow.  Tweak settings for samples chunk size

--- zounds/node/byte_depth.py
+++ zounds/node/byte_depth.py
@@ -1,33 +1,33 @@
 from __future__ import division
 
 _lookup = {
-       # https://support.microsoft.com/en-us/kb/89879
-       'MS_ADPCM' : 1,
-       'IMA_ADPCM' : 1,
-       'PCM_S8' : 1,
-       'PCM_U8' : 1,
-       'PCM_16' : 2,
-       'PCM_24' : 3,
-       'PCM_32' : 4,
-       'FLOAT'  : 4,
-       'DOUBLE' : 8,
-       # this is a guess, erring on the side of caution
-       'VORBIS' : 3
+    # https://support.microsoft.com/en-us/kb/89879
+    'MS_ADPCM': 1,
+    'IMA_ADPCM': 1,
+    'PCM_S8': 1,
+    'PCM_U8': 1,
+    'PCM_16': 2,
+    'PCM_24': 3,
+    'PCM_32': 4,
+    'FLOAT': 4,
+    'DOUBLE': 8,
+    # this is a guess, erring on the side of caution
+    'VORBIS': 3
 }
 
+
 def chunk_size_samples(sf, buf):
-    '''
-    Black magic to account for the fact that libsndfile's behavior varies 
+    """
+    Black magic to account for the fact that libsndfile's behavior varies
     depending on file format when using the virtual io api.
-    
+
     If you ask for more samples from an ogg or flac file than are available
     at that moment, libsndfile will give you no more samples ever, even if
     more bytes arrive in the buffer later.
-    '''
+    """
     byte_depth = _lookup[sf.subtype]
     channels = sf.channels
     bytes_per_second = byte_depth * sf.samplerate * channels
     secs = len(buf) / bytes_per_second
-    secs = max(4, secs - 4)
-    return int(secs * sf.samplerate)    
-        
+    secs = max(1, secs - 6)
+    return int(secs * sf.samplerate)
--- zounds/node/test_integration.py
+++ zounds/node/test_integration.py
@@ -13,7 +13,7 @@ from sliding_window import SlidingWindow, OggVorbisWindowingFunc
 from duration import Seconds, Milliseconds
 from timeseries import ConstantRateTimeSeriesFeature, TimeSlice
 from samplerate import SR44100, HalfLapped
-from basic import Max 
+from basic import Max
 
 from soundfile import SoundFile
 import numpy as np
@@ -21,150 +21,161 @@ import numpy as np
 windowing_scheme = HalfLapped()
 samplerate = SR44100()
 
-class Document(BaseModel):
 
-    raw = ByteStreamFeature(\
-        ByteStream, 
-        chunksize = 2 * 44100 * 30 * 2,
-        store = False)
+class Settings(PersistenceSettings):
+    id_provider = UuidProvider()
+    key_builder = StringDelimitedKeyBuilder()
+    database = InMemoryDatabase(key_builder=key_builder)
 
-    ogg = OggVorbisFeature(\
-        OggVorbis,
-        needs = raw,
-        store = True)
 
-    pcm = ConstantRateTimeSeriesFeature(\
-        AudioStream, 
-        needs = raw, 
-        store = False)
+class Document(BaseModel, Settings):
+    raw = ByteStreamFeature(
+            ByteStream,
+            chunksize=2 * 44100 * 30 * 2,
+            store=True)
 
-    resampled = ConstantRateTimeSeriesFeature(\
-        Resampler,
-        needs = pcm,
-        samplerate = samplerate,
-        store = True)
+    ogg = OggVorbisFeature(
+            OggVorbis,
+            needs=raw,
+            store=True)
 
-    windowed = ConstantRateTimeSeriesFeature(\
-        SlidingWindow, 
-        needs = resampled,
-        wscheme = windowing_scheme,
-        wfunc = OggVorbisWindowingFunc(),
-        store = False) 
+    pcm = ConstantRateTimeSeriesFeature(
+            AudioStream,
+            needs=raw,
+            store=True)
 
-    fft = ConstantRateTimeSeriesFeature(\
-        FFT,
-        needs = windowed,
-        store = True)
-    
-    chroma = ConstantRateTimeSeriesFeature(\
-        Chroma,
-        needs = fft,
-        samplerate = samplerate,
-        store = True)
-    
-    bark = ConstantRateTimeSeriesFeature(\
-        BarkBands,
-        needs = fft,
-        samplerate = samplerate,
-        store = True)
+    resampled = ConstantRateTimeSeriesFeature(
+            Resampler,
+            needs=pcm,
+            samplerate=samplerate,
+            store=True)
 
-    bfcc = ConstantRateTimeSeriesFeature(\
-        BFCC,
-        needs = bark,
-        store = True)
-    
-    bfcc_sliding_window = ConstantRateTimeSeriesFeature(\
-        SlidingWindow,
-        needs = bfcc,
-        wscheme = windowing_scheme * (2, 4),
-        store = True)
+    windowed = ConstantRateTimeSeriesFeature(
+            SlidingWindow,
+            needs=resampled,
+            wscheme=windowing_scheme,
+            wfunc=OggVorbisWindowingFunc(),
+            store=False)
 
-    bfcc_pooled = ConstantRateTimeSeriesFeature(\
-        Max,
-        needs = bfcc_sliding_window,
-        axis = 1,
-        store = True)
+    fft = ConstantRateTimeSeriesFeature(
+            FFT,
+            needs=windowed,
+            store=True)
+
+    chroma = ConstantRateTimeSeriesFeature(
+            Chroma,
+            needs=fft,
+            samplerate=samplerate,
+            store=True)
+
+    bark = ConstantRateTimeSeriesFeature(
+            BarkBands,
+            needs=fft,
+            samplerate=samplerate,
+            store=True)
+
+    bfcc = ConstantRateTimeSeriesFeature(
+            BFCC,
+            needs=bark,
+            store=True)
+
+    bfcc_sliding_window = ConstantRateTimeSeriesFeature(
+            SlidingWindow,
+            needs=bfcc,
+            wscheme=windowing_scheme * (2, 4),
+            store=True)
+
+    bfcc_pooled = ConstantRateTimeSeriesFeature(
+            Max,
+            needs=bfcc_sliding_window,
+            axis=1,
+            store=True)
+
 
 class HasUri(object):
-    
     def __init__(self, uri):
         super(HasUri, self).__init__()
         self.uri = uri
 
-def signal(hz = 440,seconds = 5.,sr = 44100.):
-    mono = np.random.random_sample(seconds * sr)
-    stereo = np.vstack((mono,mono)).T
-    return stereo
 
-def soundfile(hz = 440, seconds = 5., sr = 44100.):
+def signal(hz=440, seconds=5., sr=44100.):
+    # cycles per sample
+    cps = hz / sr
+    # total samples
+    ts = seconds * sr
+    mono = np.sin(np.arange(0, ts * cps, cps) * (2 * np.pi))
+    return np.vstack((mono, mono)).T
+
+
+def soundfile(hz=440, seconds=5., sr=44100.):
     bio = BytesIO()
     s = signal(hz, seconds, sr)
-    with SoundFile(\
-       bio, 
-       mode = 'w', 
-       channels = 2, 
-       format = 'WAV',
-       subtype = 'PCM_16', 
-       samplerate = int(sr)) as f:
-        for i in xrange(0, len(s), sr):
-            f.write(s[i : i + sr])
-    return HasUri(bio)
+    with SoundFile(
+            bio,
+            mode='w',
+            channels=2,
+            format='WAV',
+            subtype='PCM_16',
+            samplerate=int(sr)) as f:
+        f.write(s)
+    bio.seek(0)
+    return s, HasUri(bio)
+
 
 class IntegrationTests(unittest2.TestCase):
-    
     def setUp(self):
-        Registry.register(IdProvider, UuidProvider())
-        Registry.register(KeyBuilder, StringDelimitedKeyBuilder())
-        Registry.register(Database, InMemoryDatabase())
-        Registry.register(DataWriter, DataWriter)
-        bio = soundfile(seconds = 10.)
-        _id = Document.process(raw = bio)
+        signal, bio = soundfile(seconds=10.)
+        _id = Document.process(raw=bio)
         self.doc = Document(_id)
-        
+        self.signal = signal
+
     def test_windowed_and_fft_have_same_first_dimension(self):
         self.assertEqual(self.doc.windowed.shape[0], self.doc.fft.shape[0])
-    
+
     def test_fft_dimension_is_half_of_windowsize(self):
         self.assertEqual(self.doc.windowed.shape[1] // 2, self.doc.fft.shape[1])
-    
+
     def test_bfcc_sliding_window_has_correct_shape(self):
         self.assertEqual((4, 13), self.doc.bfcc_sliding_window.shape[1:])
-    
+
     def test_bfcc_sliding_window_has_correct_frequency(self):
-        self.assertEqual(\
-         2, self.doc.bfcc_sliding_window.frequency / self.doc.bfcc.frequency)
-    
+        self.assertEqual(
+                2,
+                self.doc.bfcc_sliding_window.frequency / self.doc.bfcc.frequency)
+
     def test_bfcc_sliding_window_has_correct_duration(self):
-        self.assertEqual(\
-         5, self.doc.bfcc_sliding_window.duration / self.doc.bfcc.frequency)
-    
+        self.assertEqual(
+                5,
+                self.doc.bfcc_sliding_window.duration / self.doc.bfcc.frequency)
+
     def test_bfcc_pooled_has_correct_shape(self):
         self.assertEqual(2, len(self.doc.bfcc_pooled.shape))
         self.assertEqual((13,), self.doc.bfcc_pooled.shape[1:])
-    
+
     def test_bfcc_pooled_has_correct_frequency(self):
-        self.assertEqual(\
-         2, self.doc.bfcc_pooled.frequency / self.doc.bfcc.frequency)
-    
+        self.assertEqual(
+                2, self.doc.bfcc_pooled.frequency / self.doc.bfcc.frequency)
+
     def test_bfcc_pooled_has_correct_duration(self):
-        self.assertEqual(\
-         5, self.doc.bfcc_pooled.duration / self.doc.bfcc.frequency)
-    
+        self.assertEqual(
+                5, self.doc.bfcc_pooled.duration / self.doc.bfcc.frequency)
+
     def test_can_get_second_long_slice_from_ogg_vorbis_feature(self):
         ogg = self.doc.ogg
-        samples = ogg[TimeSlice(Seconds(1), start = Seconds(5))]
+        samples = ogg[TimeSlice(Seconds(1), start=Seconds(5))]
         self.assertEqual(44100, len(samples))
-    
+
     def test_can_get_entirety_of_ogg_vorbis_feature_with_slice(self):
         ogg = self.doc.ogg
         samples = ogg[:]
         self.assertEqual(441000, len(samples))
-    
+
     def test_can_get_end_of_ogg_vorbis_feature_with_slice(self):
         ogg = self.doc.ogg
         samples = ogg[TimeSlice(Seconds(1), Milliseconds(9500))]
         self.assertEqual(22050, len(samples))
-    
+
     def test_sliding_window_has_correct_relationship_to_bfcc(self):
-        self.assertEqual(\
-             2, self.doc.bfcc.shape[0] // self.doc.bfcc_sliding_window.shape[0])
+        self.assertEqual(
+                2,
+                self.doc.bfcc.shape[0] // self.doc.bfcc_sliding_window.shape[0])
