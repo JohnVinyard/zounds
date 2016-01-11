71736e176238d9950298466ae60c894b3de7c943
jvinyard <jvinyard@localhost>
2016-01-10 20:30:33 -0600

    Fix bug whereby wrong frequency-wise slice was taken from short time fourier transform data.

---
+++ zounds/node/util.py
@@ -0,0 +1,12 @@
+import os
+
+
+def iter_files(base_path):
+    for fn in os.listdir(base_path):
+        yield os.path.join(base_path, fn)
+
+
+def process_dir(base_path, process_func):
+    for fp in iter_files(base_path):
+        process_func(fp)
+
