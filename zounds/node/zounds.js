f51f9a2f47c5b3cf086c149d6a4f186649b718a9
jvinyard <jvinyard@localhost>
2016-01-31 15:15:06 -0600

    Display audio slices in the UI

--- zounds/node/zounds.js
+++ zounds/node/zounds.js
@@ -16,8 +16,56 @@ $(function() {
 
     }
 
-    function Visualization(selector, bus) {
-        var el = $(selector);
+    function AudioSlice(data, root, context) {
+        var
+            self = this,
+            client = new ZoundsClient();
+
+        var
+            slice = {
+                start: data.slice.start,
+                stop: data.slice.start + data.slice.duration
+            },
+            audio = client.fetchAudio(data.audio, slice, context),
+            vis = client.fetchBinary(data.visualization, slice),
+            aggregate = Promise.all([audio, vis]);
+
+        this.play = function() {
+            var source = context.createBufferSource();
+            source.buffer = self.audio;
+            source.connect(context.destination);
+            source.start(0);
+        }
+
+        function arrayBufferToBase64(buffer) {
+            var binary = '';
+            var bytes = new Uint8Array(buffer);
+            var len = bytes.byteLength;
+            for (var i = 0; i < len; i++) {
+                binary += String.fromCharCode(bytes[i]);
+            }
+            return window.btoa(binary);
+        }
+
+        aggregate.then(function(results) {
+            var
+                audio = results[0],
+                image = results[1];
+
+            self.audio = audio;
+            $('<img>')
+                .attr('src', 'data:image/png;base64,' + arrayBufferToBase64(image))
+                .appendTo(root)
+                .click(function() {
+                    self.play();
+                });
+        });
+    }
+
+    function Visualization(selector, bus, context) {
+        var
+            el = $(selector),
+            client = new ZoundsClient();
 
         bus.subscribe(events.FEATURE_RECEIVED, function(event, data) {
             el.empty();
@@ -40,7 +88,7 @@ $(function() {
                     url: data.url,
                     dataType: 'json'
                 }).done(function(resp) {
-                    console.log(resp);
+                    new AudioSlice(resp.results[0], el, context);
                 });
             }
         });
@@ -57,8 +105,41 @@ $(function() {
                 dataType: 'json',
                 contentType: 'text/plain'
             });
-        }
+        };
 
+        this.fetchBinary = function(url, range) {
+            return new Promise(function(resolve, reject) {
+                var xhr = new XMLHttpRequest();
+                xhr.open('GET', url);
+                if(range) {
+                    xhr.setRequestHeader('Range', 'seconds=' + range.start.toString() + '-' + range.stop.toString());
+                }
+                xhr.responseType = 'arraybuffer';
+                xhr.onload = function() {
+                    if(this.status >= 200 && this.status < 300) {
+                        resolve(xhr.response);
+                    } else {
+                        reject(this.status, xhr.statusText);
+                    }
+                };
+                xhr.onerror = function() {
+                    reject(this.status, xhr.statusText);
+                };
+                xhr.send();
+            });
+        };
+
+        this.fetchAudio = function(url, range, context) {
+            return this
+                .fetchBinary(url, range)
+                .then(function(data) {
+                    return new Promise(function(resolve, reject) {
+                        context.decodeAudioData(data, function(buffer) {
+                            resolve(buffer);
+                        });
+                    });
+                });
+        }
     }
 
     function History() {
@@ -143,7 +224,8 @@ $(function() {
         });
     }
 
+    var audioContext = new AudioContext();
     var bus = new MessageBus();
     var input = new Console('#input', '#output', bus);
-    var vis = new Visualization('#visualization', bus);
+    var vis = new Visualization('#visualization', bus, audioContext);
 });
