edf4646137e2bf65a0aabd3455be208bec577485
jvinyard <jvinyard@localhost>
2016-02-15 11:55:56 -0600

    Move composite feature over into new flow-style and delete old code

--- zounds/node/basic.py
+++ zounds/node/basic.py
@@ -1,45 +1,85 @@
-from flow import Node
+from flow import Node, NotEnoughData
 import numpy as np
+from collections import OrderedDict
+from timeseries import ConstantRateTimeSeries
+
+
+class Merge(Node):
+    """
+    Combine two or more sources into a single feature
+    """
+    def __init__(self, needs=None):
+        super(Merge, self).__init__(needs=needs)
+        exc_msg = 'you must supply two or more dependencies'
+        try:
+            needs = list(needs)
+        except TypeError:
+            raise ValueError(exc_msg)
+
+        if len(needs) < 2:
+            raise ValueError(exc_msg)
+
+        self._cache = OrderedDict((id(n), None) for n in needs)
+
+    def _enqueue(self, data, pusher):
+        key = id(pusher)
+        print self._cache[key]
+        if self._cache[key] is None or self._cache[key].size == 0:
+            self._cache[key] = data
+        else:
+            self._cache[key] = self._cache[key].concatenate(data)
+
+    def _dequeue(self):
+        if any(v is None or len(v) == 0 for v in self._cache.itervalues()):
+            raise NotEnoughData()
+        shortest = min(len(v) for v in self._cache.itervalues())
+        output = OrderedDict(
+            (k, v[:shortest]) for k, v in self._cache.iteritems())
+        self._cache = OrderedDict(
+            (k, v[shortest:]) for k, v in self._cache.iteritems())
+        return output
+
+    def _process(self, data):
+        yield ConstantRateTimeSeries.concat(data.values(), axis=1)
+
 
 class Slice(Node):
-    
-    def __init__(self, sl = None, needs = None):
-        super(Slice, self).__init__(needs = needs)
+    def __init__(self, sl=None, needs=None):
+        super(Slice, self).__init__(needs=needs)
         self._sl = sl
-    
+
     def _process(self, data):
         yield data[:, self._sl]
 
+
 class Sum(Node):
-    
-    def __init__(self, axis = 0, needs = None):
-        super(Sum, self).__init__(needs = needs)
+    def __init__(self, axis=0, needs=None):
+        super(Sum, self).__init__(needs=needs)
         self._axis = axis
-    
+
     def _process(self, data):
         # TODO: This should be generalized.  Sum will have this same problem
         try:
-            data = np.sum(data, axis = self._axis)
+            data = np.sum(data, axis=self._axis)
         except ValueError:
             print 'ERROR'
             data = data
         if data.shape[0]:
             yield data
 
+
 class Max(Node):
-    
-    def __init__(self, axis = 0, needs = None):
-        super(Max, self).__init__(needs = needs)
+    def __init__(self, axis=0, needs=None):
+        super(Max, self).__init__(needs=needs)
         self._axis = axis
-    
+
     def _process(self, data):
         # TODO: This should be generalized.  Sum will have this same problem
         try:
-            data = np.max(data, axis = self._axis)
+            data = np.max(data, axis=self._axis)
         except ValueError:
             print 'ERROR'
             data = data
         if data.shape[0]:
             yield data
-        
-        
+
---
+++ zounds/node/test_merge.py
@@ -0,0 +1,189 @@
+import unittest2
+import numpy as np
+from flow import BaseModel, Node, PersistenceSettings
+from basic import Merge
+from timeseries import ConstantRateTimeSeries, ConstantRateTimeSeriesFeature
+from duration import Milliseconds
+
+
+class MergeTester(Node):
+    def __init__(
+            self,
+            total_frames=100,
+            increments_of=30,
+            features=10,
+            needs=None):
+        super(MergeTester, self).__init__(needs=needs)
+        self.features = features
+        self.increments_of = increments_of
+        self.total_frames = total_frames
+
+    def _process(self, data):
+        for i in xrange(0, self.total_frames, self.increments_of):
+            size = min(self.increments_of, self.total_frames - i)
+            yield ConstantRateTimeSeries(
+                np.zeros((size, self.features)), frequency=Milliseconds(500))
+
+
+class MergeTests(unittest2.TestCase):
+
+    def test_raises_if_single_source(self):
+
+        class Document(BaseModel, PersistenceSettings):
+            source = ConstantRateTimeSeriesFeature(
+                MergeTester,
+                store=True)
+            merged = ConstantRateTimeSeriesFeature(
+                Merge,
+                needs=source,
+                store=True)
+        self.assertRaises(ValueError, lambda: Document.process(source=''))
+
+    def test_raises_if_single_element_iterable(self):
+
+        class Document(BaseModel, PersistenceSettings):
+            source = ConstantRateTimeSeriesFeature(
+                MergeTester,
+                store=True)
+            merged = ConstantRateTimeSeriesFeature(
+                Merge,
+                needs=[source],
+                store=True)
+        self.assertRaises(ValueError, lambda: Document.process(source=''))
+
+    def test_can_combine_two_sources_at_same_rate(self):
+
+        class Document(BaseModel, PersistenceSettings):
+            source1 = ConstantRateTimeSeriesFeature(
+                MergeTester,
+                total_frames=200,
+                store=True)
+            source2 = ConstantRateTimeSeriesFeature(
+                MergeTester,
+                total_frames=200,
+                store=True)
+            merged = ConstantRateTimeSeriesFeature(
+                Merge,
+                needs=[source1, source2],
+                store=True)
+
+        _id = Document.process(source1='', source2='')
+        doc = Document(_id)
+        self.assertEqual((200, 20), doc.merged.shape)
+
+    def test_can_combine_three_sources_at_same_rate(self):
+        class Document(BaseModel, PersistenceSettings):
+            source1 = ConstantRateTimeSeriesFeature(
+                MergeTester,
+                total_frames=200,
+                store=True)
+            source2 = ConstantRateTimeSeriesFeature(
+                MergeTester,
+                total_frames=200,
+                store=True)
+            source3 = ConstantRateTimeSeriesFeature(
+                MergeTester,
+                total_frames=200,
+                store=True)
+            merged = ConstantRateTimeSeriesFeature(
+                Merge,
+                needs=(source1, source2, source3),
+                store=True)
+
+        _id = Document.process(source1='', source2='', source3='')
+        doc = Document(_id)
+        self.assertEqual((200, 30), doc.merged.shape)
+
+    def test_can_combine_two_sources_at_different_rates(self):
+        class Document(BaseModel, PersistenceSettings):
+            source1 = ConstantRateTimeSeriesFeature(
+                MergeTester,
+                total_frames=200,
+                increments_of=30,
+                store=True)
+            source2 = ConstantRateTimeSeriesFeature(
+                MergeTester,
+                total_frames=200,
+                increments_of=40,
+                store=True)
+            merged = ConstantRateTimeSeriesFeature(
+                Merge,
+                needs=(source1, source2),
+                store=True)
+
+        _id = Document.process(source1='', source2='')
+        doc = Document(_id)
+        self.assertEqual((200, 20), doc.merged.shape)
+
+    def test_can_combine_three_sources_at_different_rates(self):
+        class Document(BaseModel, PersistenceSettings):
+            source1 = ConstantRateTimeSeriesFeature(
+                MergeTester,
+                total_frames=200,
+                increments_of=12,
+                store=True)
+            source2 = ConstantRateTimeSeriesFeature(
+                MergeTester,
+                total_frames=200,
+                increments_of=17,
+                store=True)
+            source3 = ConstantRateTimeSeriesFeature(
+                MergeTester,
+                total_frames=200,
+                increments_of=32,
+                store=True)
+            merged = ConstantRateTimeSeriesFeature(
+                Merge,
+                needs=(source1, source2, source3),
+                store=True)
+
+        _id = Document.process(source1='', source2='', source3='')
+        doc = Document(_id)
+        self.assertEqual((200, 30), doc.merged.shape)
+
+    def test_shortest_of_two_sources(self):
+        class Document(BaseModel, PersistenceSettings):
+            source1 = ConstantRateTimeSeriesFeature(
+                MergeTester,
+                total_frames=190,
+                increments_of=30,
+                store=True)
+            source2 = ConstantRateTimeSeriesFeature(
+                MergeTester,
+                total_frames=200,
+                increments_of=40,
+                store=True)
+            merged = ConstantRateTimeSeriesFeature(
+                Merge,
+                needs=[source1, source2],
+                store=True)
+
+        _id = Document.process(source1='', source2='')
+        doc = Document(_id)
+        self.assertEqual((190, 20), doc.merged.shape)
+
+    def test_shortest_of_three_sources(self):
+        class Document(BaseModel, PersistenceSettings):
+            source1 = ConstantRateTimeSeriesFeature(
+                MergeTester,
+                total_frames=200,
+                increments_of=12,
+                store=True)
+            source2 = ConstantRateTimeSeriesFeature(
+                MergeTester,
+                total_frames=185,
+                increments_of=17,
+                store=True)
+            source3 = ConstantRateTimeSeriesFeature(
+                MergeTester,
+                total_frames=50,
+                increments_of=32,
+                store=True)
+            merged = ConstantRateTimeSeriesFeature(
+                Merge,
+                needs=[source1, source2, source3],
+                store=True)
+
+        _id = Document.process(source1='', source2='', source3='')
+        doc = Document(_id)
+        self.assertEqual((50, 30), doc.merged.shape)
