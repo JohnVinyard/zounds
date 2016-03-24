fdd51f311a20df88ded519275f976393dffa97fd
John Vinyard <john.vinyard@gmail.com>
2016-03-23 21:16:05 -0500

    Make an attempt to build the correct version of libsndfile from source, until the ubuntu packages are up-to-date

---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+apt-get update
+# install packages needed by libsndfile
+apt-get install -y \
+	g++ \
+	autoconf \
+	autogen \
+	automake \
+	libtool \
+	pkg-config \
+	libogg0 \
+	libogg-dev \
+	libvorbis0a \
+	libvorbis-dev
+
+# get libflac 1.3.1, build and install
+wget http://downloads.xiph.org/releases/flac/flac-1.3.1.tar.xz
+tar xf flac-1.3.1.tar.xz
+cd flac-1.3.1
+./configure && make && make install
+# get libsndfile 1.0.26, build and install
+cd ..
+wget https://github.com/erikd/libsndfile/archive/1.0.26.tar.gz
+tar -xzf 1.0.26.tar.gz
+cd libsndfile-1.0.26
+./autogen.sh
+./configure && make && make install
