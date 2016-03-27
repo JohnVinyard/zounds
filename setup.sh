384d2447b3d6d9d46d139ec2ecc977a7b577c09f
John Vinyard <john.vinyard@gmail.com>
2016-03-27 10:26:45 -0500

    Add some package dependencies that TravisCI seems to have pre-installed, but don't seem to be present on a fresh installation of Ubuntu14.04

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 apt-get update
-# install packages needed by libsndfile
+# install packages needed by libsndfile, matplotlib and lmdb
 apt-get install -y \
 	g++ \
 	autoconf \
@@ -13,8 +13,12 @@ apt-get install -y \
 	libvorbis0a \
 	libvorbis-dev \
 	libsamplerate0 \
-	libsamplerate0-dev
-
+	libsamplerate0-dev \
+	libx11-dev \
+	python-dev \
+	libfreetype6-dev \
+	libpng12-dev \
+	libffi-dev
 # get libflac 1.3.1, build and install
 wget http://downloads.xiph.org/releases/flac/flac-1.3.1.tar.xz
 tar xf flac-1.3.1.tar.xz
@@ -22,8 +26,8 @@ cd flac-1.3.1
 ./configure && make && make install
 # get libsndfile 1.0.26, build and install
 cd ..
-wget https://github.com/erikd/libsndfile/archive/1.0.26.tar.gz
-tar -xzf 1.0.26.tar.gz
-cd libsndfile-1.0.26
+wget http://www.mega-nerd.com/tmp/libsndfile-1.0.26pre5.tar.gz
+tar -xzf libsndfile-1.0.26pre5.tar.gz
+cd libsndfile-1.0.26pre5.tar.gz
 ./autogen.sh
 ./configure --prefix=/usr/bin && make && make install
