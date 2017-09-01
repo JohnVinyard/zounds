fb06d260dedaf70b6ccedc2d1b6a70f0f3c642bd
John Vinyard <john.vinyard@gmail.com>
2017-09-01 12:54:53 -0500

    Guard against missing numpy in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from setuptools import setup
 import re
-import numpy as np
-from distutils.extension import Extension
+
 
 
 try:
@@ -19,20 +18,27 @@ with open('zounds/__init__.py', 'r') as fd:
 download_url = 'https://github.com/jvinyard/zounds/tarball/{version}'\
     .format(**locals())
 
+try:
+    import numpy as np
+    from distutils.extension import Extension
 
-countbits = Extension(
-    name='countbits',
-    sources=['zounds/nputil/countbits.pyx'],
-    include_dirs=[np.get_include()],
-    extra_compile_args=[
-        '-shared',
-        '-pthread',
-        '-fPIC',
-        '-fwrapv',
-        '-O2',
-        '-Wall',
-        '-fno-strict-aliasing'
-    ])
+    countbits = Extension(
+        name='countbits',
+        sources=['zounds/nputil/countbits.pyx'],
+        include_dirs=[np.get_include()],
+        extra_compile_args=[
+            '-shared',
+            '-pthread',
+            '-fPIC',
+            '-fwrapv',
+            '-O2',
+            '-Wall',
+            '-fno-strict-aliasing'
+        ])
+    extension_modules = [countbits]
+except ImportError:
+    extension_modules = []
+
 
 setup(
         name='zounds',
@@ -42,7 +48,7 @@ setup(
         author_email='john.vinyard@gmail.com',
         long_description=long_description,
         download_url=download_url,
-        ext_modules=[countbits],
+        ext_modules=extension_modules,
         packages=[
             'zounds',
             'zounds.basic',
