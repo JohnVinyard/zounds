dcf2e87739d4c1bf99836aa20f4875ce9912f81e
John Vinyard <john.vinyard@gmail.com>
2017-09-07 21:14:04 -0500

    Try something that's frankly really icky to get readthedocs to build

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup
 import re
+import os
 
 try:
     import pypandoc
@@ -17,26 +18,29 @@ with open('zounds/__init__.py', 'r') as fd:
 download_url = 'https://github.com/jvinyard/zounds/tarball/{version}' \
     .format(**locals())
 
-try:
-    import numpy as np
-    from distutils.extension import Extension
+on_rtd = os.environ.get('READTHEDOCS') == 'True'
 
-    countbits = Extension(
-        name='countbits',
-        sources=['zounds/nputil/countbits.pyx'],
-        include_dirs=[np.get_include()],
-        extra_compile_args=[
-            '-shared',
-            '-pthread',
-            '-fPIC',
-            '-fwrapv',
-            '-O2',
-            '-Wall',
-            '-fno-strict-aliasing'
-        ])
-    extension_modules = [countbits]
-except ImportError:
-    extension_modules = []
+if not on_rtd:
+    try:
+        import numpy as np
+        from distutils.extension import Extension
+
+        countbits = Extension(
+            name='countbits',
+            sources=['zounds/nputil/countbits.pyx'],
+            include_dirs=[np.get_include()],
+            extra_compile_args=[
+                '-shared',
+                '-pthread',
+                '-fPIC',
+                '-fwrapv',
+                '-O2',
+                '-Wall',
+                '-fno-strict-aliasing'
+            ])
+        extension_modules = [countbits]
+    except ImportError:
+        extension_modules = []
 
 setup(
     name='zounds',
