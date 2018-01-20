9de02857da091721058d8bc8a236418441cca172
John Vinyard <john.vinyard@gmail.com>
2018-01-20 09:47:30 -0600

    The base learning pipeline should accept the reservoir dtype as a Var

--- zounds/learn/graph.py
+++ zounds/learn/graph.py
@@ -1,16 +1,15 @@
-import numpy as np
 import featureflow as ff
 from random_samples import ShuffledSamples
 
 
-def learning_pipeline(dtype=np.float32):
+def learning_pipeline():
     class LearningPipeline(ff.BaseModel):
         samples = ff.PickleFeature(ff.IteratorNode)
 
         shuffled = ff.PickleFeature(
             ShuffledSamples,
             nsamples=ff.Var('nsamples'),
-            dtype=dtype,
+            dtype=ff.Var('dtype'),
             needs=samples)
 
     return LearningPipeline
