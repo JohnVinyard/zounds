2e08213c0a389fe3f10f9e2d8c28e211652c8760
John Vinyard <john.vinyard@gmail.com>
2018-08-17 20:29:27 -0500

    Make it possible to pass other distance metrics to the brute force search

--- zounds/index/brute_force.py
+++ zounds/index/brute_force.py
@@ -25,29 +25,14 @@ class BaseBruteForceSearch(object):
         return self.search(query, n_results)
 
 
-# class BruteForceSearch(object):
-#     def __init__(self, gen):
-#         index = []
-#         self._ids = []
-#         for _id, example in gen:
-#             index.append(example)
-#             crts = ConstantRateTimeSeries(example)
-#             for ts, _ in crts.iter_slices():
-#                 self._ids.append((_id, ts))
-#         self.index = np.concatenate(index)
-#
-#     def random_search(self, n_results=10):
-#         query = choice(self.index)
-#         distances = cdist(query[None, ...], self.index)
-#         indices = np.argsort(distances[0])[:n_results]
-#         return SearchResults(query, (self._ids[i] for i in indices))
-
 class BruteForceSearch(BaseBruteForceSearch):
-    def __init__(self, gen):
+    def __init__(self, gen, distance_metric='euclidean'):
         super(BruteForceSearch, self).__init__(gen)
+        self.distance_metric = distance_metric
 
     def search(self, query, n_results=10):
-        distances = cdist(query[None, ...], self.index)
+        distances = cdist(
+            query[None, ...], self.index, metric=self.distance_metric)
         indices = np.argsort(distances[0])[:n_results]
         return SearchResults(query, (self._ids[i] for i in indices))
 
