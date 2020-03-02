f78ec8a5f5b1d653b12a38dc0845fe82d1e9a644
John Vinyard <john.vinyard@gmail.com>
2020-03-01 19:48:47 -0600

    Update featureflow version

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ setup(
         'zounds.util'
     ],
     install_requires=[
-        'featureflow>=3.0.2',
+        'featureflow>=3.0.3',
         'nose',
         'unittest2',
         'certifi==2017.7.27.1',
